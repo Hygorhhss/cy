2b860e64b1379c84cf4085deb908b82809bf00e9
Hygor Henrique <101516308+Hygorhhss@users.noreply.github.com>
2025-02-03 21:28:52 -0300

    complemento do curso request, invoke, timer e repeat

--- cypress/integration/avancando.spec.js
+++ cypress/integration/avancando.spec.js
@@ -10,4 +10,64 @@ describe ('avançando com o cypress', function(){
         cy.get('.button')
     })
 
-})
+
+    Cypress._.times(5, () =>{
+        it('como teste irá repetir 5 vezes', () => {
+            cy.get('#firstName').type('Hygor')
+        })
+    })
+    
+    it('Mostrar o "conteúdo" e a quantia de vezes que é para repetir', () => {
+        const longText = Cypress._.repeat ('sdfadadasda', 20)
+
+        cy.visit('./src/index.html')
+
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
+    })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+
+    })
+
+    it('preenche a area de texto usando o comando invoke', () => {
+        cy.visit('./src/index.html')
+        cy.get('#open-text-area')
+            .invoke('val', 'Preencher o campo')
+            .should('have.value', 'Preencher o campo')
+    })
+
+
+    it.only('faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .as('getRequest')
+            .its('status')
+            .should('be.equal', 200)
+
+        cy.get('@getRequest')
+            .its('statusText')
+            .should('be.equal', 'OK')
+            
+        cy.get('@getRequest')
+            .its('body')
+            .should('include', 'CAC TAT')
+    })
+
+
+    
+    })
